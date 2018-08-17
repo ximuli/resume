1bff68f2b29a93f3a37411667625e7c4618c7933
ximuli <ximuli666@163.com>
2018-08-17 15:57:26 +0800

    给 message.js 引入 MVC 思想。

--- js/message.js
+++ js/message.js
@@ -1,57 +1,76 @@
-var APP_ID = '5vrcc0vW0KdRm2JVrQc3CViF-gzGzoHsz';
-var APP_KEY = '2WWUpawSiPNJCq9xDxFUqciL';
+!function(){
+	var view = document.querySelector('section.messageBoard')
+    
+    var model = {
+    	init: function() {
+    		var APP_ID = '5vrcc0vW0KdRm2JVrQc3CViF-gzGzoHsz'
+			var APP_KEY = '2WWUpawSiPNJCq9xDxFUqciL'
+			AV.init({ appId: APP_ID, appKey: APP_KEY })
+    	},
+    	//获取数据
+    	fetch: function() {
+            var query = new AV.Query('Message')
+			return query.find()  // Promise 对象
+    	},
+    	//创建数据
+    	save: function(name, content) {
+            var Message = AV.Object.extend('Message');
+			var messgae = new Message();
+			return messgae.save({  //返回 Promise 对象
+				'name':name,
+		        'content': content
+		    })
+    	}
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
+    }
 
-var query = new AV.Query('Message')
-query.find()
-    .then(
-    	function (messages) {
+    var controller = {
+    	view: null,
+    	model: null,
+    	messageList: null,
+    	init: function(view, model) {
+    		this.view = view
+    		this.model = model
     		
-		    let array = messages.map((item) => item.attributes)
-		    
-		    array.forEach((item) => {
+    		this.messageList = view.querySelector('#messageList')
+    		this.form = view.querySelector('#postMessage')
+    		this.model.init()
+    		this.loadMessages()
+    		this.bindEvents()
+    	},
+    	loadMessages: function() {
+		    this.model.fetch().then(
+		    	(messages) => {
+				    let array = messages.map((item) => item.attributes)
+				    array.forEach((item) => {
+				        let li = document.createElement('li')
+				    	li.innerText = `${item.name}说: ${item.content}`
+				    	this.messageList.appendChild(li)
+				    })
+		        }
+		    )
+    	},
+    	bindEvents: function() {
+			this.form.addEventListener('submit', function(e) {
+				e.preventDefault()
+				this.saveMessage()
+			})
+    	},
+    	saveMessage: function() {
+    		let myForm = this.form
+    		let name = myForm.querySelector('input[name=name]').value
+			let content = myForm.querySelector('input[name=content]').value
+			this.model.save(name, content).then(function(object) {
 		        let li = document.createElement('li')
-		    	li.innerText = `${item.name}说: ${item.content}`
+		    	li.innerText = `${object.attributes.name}说: ${object.attributes.content}`
 		    	let messageList = document.querySelector('#messageList')
 		    	messageList.appendChild(li)
+		    	myForm.querySelector('input[name=content]').value = ''
+		        console.log(object)
 		    })
-        }
-    )
+    	}
+    }
 
+    controller.init(view, model)
 
-let myForm = document.querySelector('#postMessage')
-myForm.addEventListener('submit', function(e) {
-	e.preventDefault()
-	let name = myForm.querySelector('input[name=name]').value
-	let content = myForm.querySelector('input[name=content]').value
-	var Message = AV.Object.extend('Message');
-	var messgae = new Message();
-	messgae.save({
-		'name':name,
-        'content': content
-    }).then(function(object) {
-        let li = document.createElement('li')
-    	li.innerText = `${object.attributes.name}说: ${object.attributes.content}`
-    	let messageList = document.querySelector('#messageList')
-    	messageList.appendChild(li)
-    	myForm.querySelector('input[name=content]').value = ''
-        console.log(object)
-    })
-})
-
-
-
-
-
-
-// var TestObject = AV.Object.extend('TestObject');
-// var testObject = new TestObject();
-// testObject.save({
-//   words: 'Hello World!'
-// }).then(function(object) {
-//   alert('LeanCloud Rocks!');
-// })
+}.call()
