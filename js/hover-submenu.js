3a9942936eebcdc3d12f3a4ecf8528255a142c4c
ximuli <ximuli666@163.com>
2018-09-26 08:34:08 +0800

    移除导航二级菜单

--- js/hover-submenu.js
+++ js/hover-submenu.js
@@ -4,29 +4,28 @@
 		view: null,
 		init: function(view) {
 			this.view = view
-            this.bindEvents()
+      this.bindEvents()
 		},
 		bindEvents: function() {
 			var view = this.view
 			let liTags = view.querySelectorAll('nav > ul > li')
 			for (let i = 0; i < liTags.length; i++) {
-			    liTags[i].onmouseenter =  (x) => {
-			        this.active(x)
-			    }
-
-			    liTags[i].onmouseleave = (x) => {
-			        this.deactive(x)
-			    }
+		    liTags[i].onmouseenter =  (x) => {
+		       this.active(x)
+		    }
+		    liTags[i].onmouseleave = (x) => {
+		       this.deactive(x)
+		    }
 			}
 		},
 		active: function(x) {
-            x.currentTarget.classList.add('active')
+        x.currentTarget.classList.add('active')
 		},
 		deactive: function(x) {
-            x.currentTarget.classList.remove('active')
+        x.currentTarget.classList.remove('active')
 		}
 	}
-	
+
 	controller.init(view)
 
 }.call()
